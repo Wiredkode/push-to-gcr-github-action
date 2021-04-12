57e4c6089bc3c7204882f307da56ebe9d10bc02d
Nuno Ribeiro <nfribeiro@gmail.com>
2021-04-12 15:37:40 +0100

    .

--- entrypoint.sh
+++ entrypoint.sh
@@ -52,6 +52,8 @@ if [ ! -z "$INPUT_BUILD_ARGS" ]; then
     BUILD_PARAMS="$BUILD_PARAMS --build-arg ${ARG}"
   done
 fi
+echo "$INPUT_REGISTRY"
+echo "$INPUT_SECRET_KEY_ID"
 if [ ! -z "$INPUT_SECRET_KEY_ID" ]; then
     echo $INPUT_SECRET_KEY_VAL > secret.secret
     BUID_PARAMS="$BUILD_PARAMS --secret id=$INPUT_SECRET_KEY_ID,src=secret.secret"
